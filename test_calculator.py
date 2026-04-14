6bd857cef66fb680d301c471acb714ae01514aee
Aaron Robertson <aaronhoover26@gmail.com>
2026-04-13 22:48:53 -0400

    kaizen update, calculator tests

--- test_calculator.py
+++ test_calculator.py
@@ -1,28 +1,32 @@
+#https://github.com/Phoenixx1st/lab11-AH-CC.git
+# Partner 1: Aaron Robertson
+# Partner 2: Camila Cabello
 import unittest
+from calculator import *
 
 class TestCalculator(unittest.TestCase):
     ######### Partner 2
     def test_add(self): # 3 assertions
-        self.assertTrue(add(1,2))
-        self.assertTrue(add(3,4))
-        self.assertTrue(add(5,6))
+        self.assertTrue(add(1,2), 3)
+        self.assertTrue(add(-5,4), -1)
+        self.assertTrue(add(0.5,0.9), 1.4)
 
 
     def test_subtract(self): # 3 assertions
-        self.assertTrue(sub(1,2))
-        self.assertTrue(sub(3,4))
-        self.assertTrue(sub(5,6))
+        self.assertTrue(subtract(1,2), -1)
+        self.assertTrue(subtract(10,5), 5)
+        self.assertTrue(subtract(1.5, 0.8), 0.7)
 
     ######## Partner 1
     def test_multiply(self):  # 3 assertions
-        self.assertEqual(multiply(3, 5), 15)
-        self.assertEqual(multiply(-2, 10), 20)
-        self.assertAlmostEqual(multiply(0.5, 4.0), 2.0)
+        self.assertEqual(mul(3, 5), 15)
+        self.assertEqual(mul(-2, 10), -20)
+        self.assertAlmostEqual(mul(0.5, 4.0), 2.0)
 
     def test_divide(self):  # 3 assertions
-        self.assertEqual(divide(3, 15), 5)
-        self.assertEqual(divide(-2, 10), -5.0)
-        self.assertAlmostEqual(divide(0.5, 2), 4.0)
+        self.assertEqual(div(3, 15), 5)
+        self.assertEqual(div(-2, 10), -5.0)
+        self.assertAlmostEqual(div(0.5, 2), 4.0)
 
     def test_divide_by_zero(self): # 1 assertion
         with self.assertRaises(ZeroDivisionError):
@@ -30,14 +34,14 @@ class TestCalculator(unittest.TestCase):
     
 
     def test_logarithm(self): # 3 assertions
-        self.assertTrue(log(5,6))
-        self.assertTrue(log(7,8))
-        self.assertTrue(log(9,10))
+        self.assertTrue(logarithm(2,8), 3.0)
+        self.assertTrue(logarithm(10, 100), 2.0)
+        self.assertAlmostEqual(logarithm(10, 8), 0.90309, places=5)
 
 
     def test_log_invalid_base(self): # 1 assertion
         with self.assertRaises(ValueError):
-            log(1, 2)
+            logarithm(1, 2)
 
     ######## Partner 1
     def test_log_invalid_argument(self):  # 1 assertion
@@ -54,7 +58,7 @@ class TestCalculator(unittest.TestCase):
             square_root(-1)
 
         self.assertEqual(square_root(9), 3.0)
-        self.assertAlmostEqual(2), 1.412135624)
+        self.assertAlmostEqual(square_root(2), 1.41421356, places=7)
 
 if __name__ == "__main__":
     unittest.main()
