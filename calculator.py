fd436555994612159e3b68d2671934a634f78acf
Aaron Robertson <aaronhoover26@gmail.com>
2026-04-13 20:48:57 -0400

    modified calculator

--- calculator.py
+++ calculator.py
@@ -1,9 +1,21 @@
 import math
 
-
-
-
 # Calculator functions
+def square_root(a):
+    try:
+        if a < 0:
+            raise ValueError("Square root error, a must be positive")
+        return math.sqrt(a)
+    except Exception as e:
+        return f"Error: {e}"
+
+def hypotenuse(a, b):
+    try:
+        return math.hypot(a, b)
+    except Exception as e:
+        return f"Error: {e}"
+
+
 def add(a, b):
     return a + b
 
@@ -19,7 +31,7 @@ def divide(a, b):
     return b / a
 
 def logarithm(a, b):
-    if a<= 1 or a == 1:
+    if a<= 0 or a == 1:
         raise ValueError("Logarithmic error, base must be greater than 1")
     if b <= 0:
         raise ValueError("Logarithmic error, must be greater than 0")
